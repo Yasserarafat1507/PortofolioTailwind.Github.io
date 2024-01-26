2b1a7d88ed8dbd725fea083b7bf15e3d9e4c6379
Yasser Arafah <yasserarafat@gmail.com>
2024-01-26 14:13:05 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/**/*.{html,js}"],
+  theme: {
+    container : {
+      center : true,
+      padding : '16px',
+    },
+    extend: {
+      colors : {
+        penting : '#14b8a6',
+        dark : '#0f172a',
+        text : '#64748b',
+      },
+      screens : {
+        '2xl' : '1320px',
+      },
+    },
+  },
+  plugins: [],
+}
+
