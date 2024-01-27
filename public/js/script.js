b9a43b77d2c327bc1df300772e40004cc47e1213
Yasser Arafah <yasserarafat@gmail.com>
2024-01-28 06:47:41 +0700

    second commit

--- public/js/script.js
+++ public/js/script.js
@@ -2,11 +2,16 @@
 window.onscroll = function() {
     const header = document.querySelector('header');
     const fixedNav = header.offsetTop;
+    const toTop = document.querySelector('#to-top');
 
     if (window.pageYOffset > fixedNav) {
         header.classList.add('navbar-fixed');
+        toTop.classList.remove('hidden');
+        toTop.classList.add('flex');
     } else {
         header.classList.remove('navbar-fixed');
+        toTop.classList.remove('flex');
+        toTop.classList.add('hidden');
     }
 }
 
@@ -18,3 +23,32 @@ humberger.addEventListener('click', function(){
     humberger.classList.toggle('humberger-active');
     navMenu.classList.toggle('hidden');
 })
+
+// klik di luar hamburger
+window.addEventListener('click', function (e) {
+    if(e.target != humberger && e.target != navMenu) {
+        humberger.classList.remove('humberger-active');
+        navMenu.classList.add('hidden');
+    }
+});
+
+// dark mode
+const checkbox = document.querySelector('#dark-toggel');
+const html = document.querySelector('html');
+
+checkbox.addEventListener('click', function() {
+    if (checkbox.checked) {
+        html.classList.add('dark');
+    }else{
+        html.classList.remove('dark');
+    }
+});
+
+// pindahkan toggel sesuai mode
+if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+        document.documentElement.classList.add('dark')
+        checkbox.checked = true;
+    } else {
+        document.documentElement.classList.remove('dark')
+        checkbox.checked = false;
+    }
